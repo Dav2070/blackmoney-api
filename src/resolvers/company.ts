90a5d897f7dbac0b400543119746f7d3868601f0
David Zurek <zurek.dav@gmail.com>
2025-12-22 15:21:38 +0100

    Update createCompany mutation to create default data for menus, categories, products, variations and offers

--- src/resolvers/company.ts
+++ src/resolvers/company.ts
@@ -1,4 +1,4 @@
-import { Company, Restaurant, User } from "@prisma/client"
+import { PrismaClient, Company, Restaurant, User } from "@prisma/client"
 import { apiErrors } from "../errors.js"
 import { List, ResolverContext } from "../types.js"
 import {
@@ -61,10 +61,13 @@ export async function createCompany(
 		}
 	})
 
+	const restaurant = company.restaurants[0]
+	await createDefaultDataForRestaurant(context.prisma, restaurant)
+
 	// Create the default register for the restaurant
 	await createRegisterForRestaurant(
 		context.prisma,
-		company.restaurants[0].id,
+		restaurant.id,
 		"Hauptkasse"
 	)
 
@@ -115,3 +118,232 @@ export async function users(
 		items
 	}
 }
+
+//#region Helper functions
+async function createDefaultDataForRestaurant(
+	prisma: PrismaClient,
+	restaurant: Restaurant
+) {
+	// Create default rooms
+	await prisma.room.createMany({
+		data: [
+			{ name: "1", restaurantId: restaurant.id },
+			{ name: "2", restaurantId: restaurant.id },
+			{ name: "3", restaurantId: restaurant.id }
+		]
+	})
+
+	// Create default menu
+	const menu = await prisma.menu.create({
+		data: {
+			restaurantId: restaurant.id
+		}
+	})
+
+	// Create default category
+	const [categoryPizza, categoryAlkoholfrei, categoryAktionen, categoryMenus] =
+		await prisma.category.createManyAndReturn({
+			data: [
+				{
+					name: "Pizza",
+					menuId: menu.id
+				},
+				{
+					name: "Alkoholfrei",
+					menuId: menu.id
+				},
+				{
+					name: "Aktionen",
+					menuId: menu.id
+				},
+				{
+					name: "Menüs",
+					menuId: menu.id
+				}
+			]
+		})
+
+	// Create default products
+	const [
+		productMargherita,
+		productSalami,
+		productCola,
+		productFanta,
+		productMittagsSpecial,
+		productPizzaMenu
+	] = await prisma.product.createManyAndReturn({
+		data: [
+			{
+				name: "Pizza Margherita",
+				price: 850,
+				type: "FOOD",
+				categoryId: categoryPizza.id
+			},
+			{
+				name: "Pizza Salami",
+				price: 990,
+				type: "FOOD",
+				categoryId: categoryPizza.id
+			},
+			{
+				name: "Cola",
+				price: 280,
+				type: "DRINK",
+				categoryId: categoryAlkoholfrei.id
+			},
+			{
+				name: "Fanta",
+				price: 280,
+				type: "DRINK",
+				categoryId: categoryAlkoholfrei.id
+			},
+			{
+				name: "Mittags-Special",
+				price: 0,
+				type: "SPECIAL",
+				categoryId: categoryAktionen.id
+			},
+			{
+				name: "Pizza-Menü",
+				price: 0,
+				type: "MENU",
+				categoryId: categoryMenus.id
+			}
+		]
+	})
+
+	// Create default variations
+	const [
+		variationSizeMargherita,
+		variationSizeSalami,
+		variationSizeCola,
+		variationSizeFanta
+	] = await prisma.variation.createManyAndReturn({
+		data: [
+			{ name: "Größe" },
+			{ name: "Größe" },
+			{ name: "Größe" },
+			{ name: "Größe" }
+		]
+	})
+
+	// Create default variation items
+	await prisma.variationItem.createMany({
+		data: [
+			{
+				name: "Klein",
+				additionalCost: 0,
+				variationId: variationSizeMargherita.id
+			},
+			{
+				name: "Groß",
+				additionalCost: 200,
+				variationId: variationSizeMargherita.id
+			},
+			{
+				name: "Klein",
+				additionalCost: 0,
+				variationId: variationSizeSalami.id
+			},
+			{
+				name: "Groß",
+				additionalCost: 250,
+				variationId: variationSizeSalami.id
+			},
+			{
+				name: "0,3L",
+				additionalCost: 0,
+				variationId: variationSizeCola.id
+			},
+			{
+				name: "0,5L",
+				additionalCost: 70,
+				variationId: variationSizeCola.id
+			},
+			{
+				name: "0,3L",
+				additionalCost: 0,
+				variationId: variationSizeFanta.id
+			},
+			{
+				name: "0,5L",
+				additionalCost: 70,
+				variationId: variationSizeFanta.id
+			}
+		]
+	})
+
+	// Create default offers for specials & menus
+	const [mittagsSpecialOffer, pizzaMenuOffer] =
+		await prisma.offer.createManyAndReturn({
+			data: [
+				{
+					menuId: menu.id,
+					productId: productMittagsSpecial.id,
+					offerType: "DISCOUNT",
+					discountType: "PERCENTAGE",
+					offerValue: 20,
+					startTime: "11:00",
+					endTime: "14:00",
+					weekdays: [
+						"MONDAY",
+						"TUESDAY",
+						"WEDNESDAY",
+						"THURSDAY",
+						"FRIDAY"
+					]
+				},
+				{
+					menuId: menu.id,
+					productId: productPizzaMenu.id,
+					offerType: "FIXED_PRICE",
+					offerValue: 1490,
+					weekdays: [
+						"MONDAY",
+						"TUESDAY",
+						"WEDNESDAY",
+						"THURSDAY",
+						"FRIDAY",
+						"SATURDAY",
+						"SUNDAY"
+					]
+				}
+			]
+		})
+
+	// Create default offer items
+	// Here using $transaction, as connect does not work with createMany
+	await prisma.$transaction([
+		prisma.offerItem.create({
+			data: {
+				offerId: mittagsSpecialOffer.id,
+				name: "Hauptgericht",
+				maxSelections: 1,
+				products: {
+					connect: [{ id: productMargherita.id }, { id: productSalami.id }]
+				}
+			}
+		}),
+		prisma.offerItem.create({
+			data: {
+				offerId: pizzaMenuOffer.id,
+				name: "Pizza",
+				maxSelections: 1,
+				products: {
+					connect: [{ id: productMargherita.id }, { id: productSalami.id }]
+				}
+			}
+		}),
+		prisma.offerItem.create({
+			data: {
+				offerId: pizzaMenuOffer.id,
+				name: "Getränk",
+				maxSelections: 1,
+				products: {
+					connect: [{ id: productCola.id }, { id: productFanta.id }]
+				}
+			}
+		})
+	])
+}
+//#endregion
